1a523669c1a056bf9580c13cda09e3e17faf4853
Richard Bailey <richard@richardbailey.xyz>
2019-01-13 22:43:29 +0000

    Updates

---
+++ precache-manifest.597692c1d98c64f2b14d3574618a7980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ad6fe3d7c569413d9148",
+    "url": "/static/js/main.ad6fe3d7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "ad6fe3d7c569413d9148",
+    "url": "/static/css/main.261115ea.chunk.css"
+  },
+  {
+    "revision": "ffdf842f83141358065e0fba22c275f8",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.626cd1e8016d08f8c1ba68207cd12f9a.js"
+  "/precache-manifest.597692c1d98c64f2b14d3574618a7980.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
