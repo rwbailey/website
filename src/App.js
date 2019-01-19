58208b6e825f7609e48f48f987cd969ec9160966
Richard Bailey <richard@richardbailey.xyz>
2019-01-19 23:59:43 +0000

    edit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello there
+            Hello there, friends.
           </p>
           <a
             className="App-link"
