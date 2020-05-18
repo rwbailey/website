b324e500cf3c56659454920e832477f13066b4c0
Richard Bailey <richard@richardbailey.xyz>
2020-05-18 13:27:13 +0100

    Add files via upload

---
+++ precache-manifest.c9917303fa41dc717874ba623146c040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71276c793da44c50a723b0ba3d72819a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cb5ed8fe32e8d59dcca",
+    "url": "/static/css/main.9de04a14.chunk.css"
+  },
+  {
+    "revision": "5c0baa47eea69766a237",
+    "url": "/static/js/2.5b06a3fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5b06a3fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb5ed8fe32e8d59dcca",
+    "url": "/static/js/main.7ed80724.chunk.js"
+  },
+  {
+    "revision": "a1239e0660564298c39e",
+    "url": "/static/js/runtime-main.4a8832b4.js"
+  }
+]);
